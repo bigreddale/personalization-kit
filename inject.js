f7d7f4acdbac9a9e15a846752e84cf154cc6bea2
Dale Gillespie <m735449@a3836594.federated.fds>
2014-12-18 11:23:11 -0800

    Trying injection model

---
+++ inject.js
@@ -0,0 +1,5 @@
+  var script=document.createElement('script');
+  script.type='text/javascript';
+  script.src = 'https://raw.githubusercontent.com/bigreddale/personalization-kit/master/widget.js';
+
+  $("head").append(script);
--- widget.js
+++ widget.js
@@ -1,14 +1,53 @@
-$(document).ready(function(){
-  var facet,
-      facets = [
-$('a[href*="INSEAM_SIZE\=36"]').parent().next(),
-$('a[href*="Inseam_size/36"]').parent().next()
-                ];
   
-  for(i = 0; i < facets.length; i++) {
-    facet = facets[i];
-    if(!facet.hasClass("selected")) {
-      facet.children().click();
-    }
-  }
+  
+$(document).ready(function(){
+  var P13NEngine = {};
+  
+  P13NEngine.init = function() {
+    var facet,
+        facets = [
+  $('a[href*="INSEAM_SIZE\=36"]').parent().next(),
+  $('a[href*="Inseam_size/36"]').parent().next()
+                  ];
+    
+    if($('#facets').length) {
+      if(!$('#PERSONALIZATION').length) {
+        $('#facets').prepend('<div class="facet selected"> \
+            <div class="facet-name"> \
+              <h2 id="PERSONALIZATION">Shop For</h2>\
+            </div>\
+            <div class="clear-all">clear</div> \
+            <div class="clearFloats"></div> \
+            <ul class="defaultFacet" height="120" style="height: auto;"> \
+            <li> \
+            <span class="PERSONALIZATION">John Doe</span>\
+            <span id="edit_PERSONALIZATION_John_Doe" class="facet-item-count">(<a href="#">edit</a>)</span> \
+            </li> \
+            </ul>\
+            </div>');
+        
+      }
+      
+      $("#PERSONALIZATION").parent().parent().find("ul").on('mouseenter mouseleave', 'li', P13NEngine.highlight);
+    
+      for(i = 0; i < facets.length; i++) {
+        facet = facets[i];
+        if(!facet.hasClass("selected")) {
+          facet.children().click();
+        }
+      }
+    };
+    
+    P13NEngine.highlight = function(obj) {
+      console.log(obj);
+      $(obj).parent().toggleClass('active-facet-list');
+      $(obj).toggleClass('facet-item-highlight');
+    };
+      
+  
+  };
+ 
+  P13NEngine.init();
+  
+
 });
