69fb739d7f1eed8b64e86a2f8c95ac20b2fe1979
Dale Gillespie <m735449@a3836594.federated.fds>
2015-02-16 17:04:30 -0800

    NEw injector

---
+++ inject2.js
@@ -0,0 +1,35 @@
+var COOKIE;
+require(["cookie"],function(c){COOKIE=c});
+
+function setZip(zip) {
+  cookies = new Array(); 
+  cookies = document.cookie.split('; '); 
+  var isNew = true; 
+  for(i=0; i<cookies.length; i++) { 
+    names = new Array(); 
+    names = cookies[i].split('='); 
+    if(names[0] == 'MISCGCs') {
+      isNew = false; values = new Array(); 
+      values = names[1].split('USERPC1_92_'); 
+      void(document.cookie="MISCGCs = " + values[0] + "USERPC1_92_" + zip  +  values[1].substring(5,values[1].length+1) + " ; path=/;"  );
+    } 
+  } 
+  if(isNew == true) { 
+    void(document.cookie="MISCGCs = USERPC1_92_" + zip + "; path=/; domain=.fds.com;"); 
+  } 
+  alert(document.cookie);
+}
+
+function setSegment(seg) {
+  document.cookie='SEGMENT='+encodeURIComponent('%7B%22EXPERIMENT%22%3A%5B'+seg+'%5D%7D')+'; path=/;';
+  alert('set segment: ' + seg) ;
+  
+}
+function getSegment() {
+  alert(COOKIE.get('SEGMENT'));
+}
+function getZip(){
+  alert(COOKIE.get('MISCGCs'));
+}
+
+
