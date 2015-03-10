5189c593acb03aa5bea5a1e768a575ebf1b90b28
Dale Gillespie <m735449@a3836594.federated.fds>
2015-03-10 16:05:47 -0700

    hack

---
+++ inject-hack.js
@@ -0,0 +1,35 @@
+
+var permissionForm = new function () {
+  
+  var me = this;
+  
+  
+
+  
+  me.init = function () {
+    if (EventHelpers.hasPageLoadHappened(arguments)) {
+      return;
+    }
+    
+    var dragNodes = cssQuery('[draggable=true]');
+    
+    for (var i=0; i<dragNodes.length; i++) {
+      EventHelpers.addEvent(dragNodes[i], 'dragend', userDragEndEvent);
+    }
+  };
+  
+  function userDragEndEvent(e) {  
+    console.log(e.target);
+    var x = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+    var y = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+    parent.postMessage({"x":x,"y":y, "src":e.target.src},'*');
+  }
+  
+  
+
+  
+  
+}
+
+DragDropHelpers.fixVisualCues=true;
+EventHelpers.addPageLoadEvent('permissionForm.init');
